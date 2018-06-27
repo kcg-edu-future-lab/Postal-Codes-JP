6ecd8719a7b9fbd73b4461c5f8291ea5d19c97eb
Keiho Sakapon <git@saka-pon.net>
2018-06-27 12:07:12 +0900

    Add controller

---
+++ PostalCodes/PostalCodesWebApi/Controllers/PrefecturesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PostalCodesWebApi.Models;
+
+namespace PostalCodesWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class PrefecturesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Prefecture> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{code:regex(^[[0-9]]{{2}}$)}")]
+        public Prefecture Get(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("ByName/{name}")]
+        public IEnumerable<Prefecture> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("ByKana/{kana}")]
+        public IEnumerable<Prefecture> GetByKana(string kana)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PostalCodes/PostalCodesWebApi/Models/PostalCodesModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PostalCodesWebApi.Models
+{
+    public class Prefecture
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Kana { get; set; }
+    }
+
+    public class City
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Kana { get; set; }
+        public Prefecture Prefecture { get; set; }
+    }
+}
