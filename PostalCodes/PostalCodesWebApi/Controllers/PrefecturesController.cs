f9d69d5ad6b5454d8a7bb52316d9d149a23be6e5
Keiho Sakapon <git@saka-pon.net>
2018-06-27 15:51:31 +0900

    Add comments

--- PostalCodes/PostalCodesWebApi/Controllers/PrefecturesController.cs
+++ PostalCodes/PostalCodesWebApi/Controllers/PrefecturesController.cs
@@ -8,16 +8,28 @@ using PostalCodesWebApi.Models;
 
 namespace PostalCodesWebApi.Controllers
 {
+    /// <summary>
+    /// 都道府県のデータを取得します。
+    /// </summary>
     [Produces("application/json")]
     [Route("api/[controller]")]
     public class PrefecturesController : Controller
     {
+        /// <summary>
+        /// すべての都道府県のリストを取得します。
+        /// </summary>
+        /// <returns>都道府県のリスト。</returns>
         [HttpGet]
         public IEnumerable<Prefecture> Get()
         {
             return PostalCodesModel.Prefectures;
         }
 
+        /// <summary>
+        /// 都道府県コードを指定して、都道府県を取得します。
+        /// </summary>
+        /// <param name="code">都道府県コード。</param>
+        /// <returns>都道府県。</returns>
         [HttpGet("{code:regex(^[[0-9]]{{2}}$)}")]
         [ProducesResponseType(200, Type = typeof(Prefecture))]
         [ProducesResponseType(404)]
@@ -28,6 +40,11 @@ namespace PostalCodesWebApi.Controllers
             return Ok(PostalCodesModel.PrefecturesMap[code]);
         }
 
+        /// <summary>
+        /// 名前を指定して、都道府県のリストを取得します。
+        /// </summary>
+        /// <param name="name">名前。</param>
+        /// <returns>都道府県のリスト。</returns>
         [HttpGet("ByName/{name}")]
         public IEnumerable<Prefecture> GetByName(string name)
         {
@@ -36,6 +53,11 @@ namespace PostalCodesWebApi.Controllers
             return PostalCodesModel.Prefectures.Where(x => x.Name.Contains(name));
         }
 
+        /// <summary>
+        /// かなを指定して、都道府県のリストを取得します。
+        /// </summary>
+        /// <param name="kana">かな。</param>
+        /// <returns>都道府県のリスト。</returns>
         [HttpGet("ByKana/{kana}")]
         public IEnumerable<Prefecture> GetByKana(string kana)
         {
