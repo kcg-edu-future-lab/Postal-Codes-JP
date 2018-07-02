ad89adfe6afc9ebd9361752a21f23e49e101d528
Keiho Sakapon <git@saka-pon.net>
2018-07-02 19:10:26 +0900

    Load original ZIP file

---
+++ PostalCodes/PostalCodesDataConsole/DataZipFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using Bellona.IO;
+
+namespace PostalCodesDataConsole
+{
+    public static class DataZipFile
+    {
+        public static string[][] FromOriginal(string originalZipPath)
+        {
+            using (var zip = ZipFile.OpenRead(originalZipPath))
+            {
+                var entry = zip.GetEntry("KEN_ALL.CSV") ?? zip.Entries.FirstOrDefault();
+                if (entry == null) return new string[0][];
+
+                using (var stream = entry.Open())
+                {
+                    return CsvFile.ReadRecordsByArray(stream, false, TextFile.ShiftJIS).ToArray();
+                }
+            }
+        }
+    }
+}
--- PostalCodes/PostalCodesDataConsole/Program.cs
+++ PostalCodes/PostalCodesDataConsole/Program.cs
@@ -4,9 +4,12 @@ namespace PostalCodesDataConsole
 {
     class Program
     {
+        const string KenAll_Local_Path = @"..\..\..\..\..\Data\Original\201805\ken_all.zip";
+        const string KenAll_JP_Uri = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var originalData = DataZipFile.FromOriginal(KenAll_Local_Path);
         }
     }
 }
