bd0e71f0b301697f8cc608db0132ae02dae1dd0a
Keiho Sakapon <git@saka-pon.net>
2018-07-02 21:54:34 +0900

    Transform original data

---
+++ PostalCodes/PostalCodesDataConsole/DataCreator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Bellona.IO;
+using Microsoft.VisualBasic;
+
+namespace PostalCodesDataConsole
+{
+    public static class DataCreator
+    {
+        public static void CreateDataZipFile(string[][] originalData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void CreateDataCsvFiles(string[][] originalData)
+        {
+            CsvFile.WriteRecordsByArray("Prefs.csv", CreatePrefs(originalData), new[] { "Code", "Name", "Kana" });
+            CsvFile.WriteRecordsByArray("Cities.csv", CreateCities(originalData), new[] { "PrefCode", "Code", "Name", "Kana" });
+            CsvFile.WriteRecordsByArray("Towns.csv", CreateTowns(originalData), new[] { "CityCode", "PostalCode", "Name", "Kana", "Remarks" });
+        }
+
+        public static IEnumerable<string[]> CreatePrefs(string[][] originalData)
+        {
+            return originalData
+                .GroupBySequentially(l => l[6])
+                .Select(g => g.First())
+                .Select(l => new[] { l[0].Substring(0, 2), l[6], l[3].ToHiragana() });
+        }
+
+        public static IEnumerable<string[]> CreateCities(string[][] originalData)
+        {
+            return originalData
+                .GroupBySequentially(l => l[0])
+                .Select(g => g.First())
+                .Select(l => new[] { l[0].Substring(0, 2), l[0], l[7], l[4].ToHiragana() });
+        }
+
+        public static IEnumerable<string[]> CreateTowns(string[][] originalData)
+        {
+            var remarksPattern = new Regex("（(.+)）$");
+
+            return originalData
+                .GroupBySequentially(l => l[2])
+                .Select(g => (g.Count() > 1 && g.First()[8].Contains("（") && !g.First()[8].Contains("）")) ? ConcatLines(g.ToArray()) : g.ToArray())
+                .SelectMany(g => g)
+                .Select(l => new[] { l[0], l[2], l[8], l[5].ToHiragana(), "" })
+                .Do(l =>
+                {
+                    if (l[2] == "以下に掲載がない場合")
+                    {
+                        l[4] = "お探しの町域が見つからない場合";
+                        l[2] = "";
+                        l[3] = "";
+                    }
+                })
+                .Do(l =>
+                {
+                    if (l[2].Contains("番地がくる場合"))
+                    {
+                        l[4] = l[2];
+                        l[2] = "";
+                        l[3] = "";
+                    }
+                })
+                .Do(l =>
+                {
+                    if (l[2].Length > 2 && l[2].Contains("一円"))
+                    {
+                        l[4] = l[2];
+                        l[2] = "";
+                        l[3] = "";
+                    }
+                })
+                .Do(l =>
+                {
+                    var nameMatch = remarksPattern.Match(l[2]);
+                    if (nameMatch.Success)
+                    {
+                        l[4] = nameMatch.Groups[1].Value;
+                        l[2] = l[2].Remove(nameMatch.Index);
+                    }
+                    var kanaMatch = remarksPattern.Match(l[3]);
+                    if (kanaMatch.Success)
+                    {
+                        l[3] = l[3].Remove(kanaMatch.Index);
+                    }
+                });
+        }
+
+        static string[][] ConcatLines(string[][] lines)
+        {
+            var line = lines[0];
+            line[5] = lines.Select(l => l[5]).Aggregate((s1, s2) => (s1 == s2) ? s1 : s1 + s2);
+            line[8] = lines.Select(l => l[8]).Aggregate((s1, s2) => s1 + s2);
+            return new[] { line };
+        }
+
+        static string ToHiragana(this string value) =>
+            Strings.StrConv(value, VbStrConv.Wide | VbStrConv.Hiragana);
+    }
+}
--- PostalCodes/PostalCodesDataConsole/Program.cs
+++ PostalCodes/PostalCodesDataConsole/Program.cs
@@ -5,11 +5,14 @@ namespace PostalCodesDataConsole
     class Program
     {
         const string KenAll_Local_Path = @"..\..\..\..\..\Data\Original\201805\ken_all.zip";
+        const string Kyoto_Local_Path = @"..\..\..\..\..\Data\Original\201805\26kyouto.zip";
         const string KenAll_JP_Uri = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip";
 
         static void Main(string[] args)
         {
             var originalData = DataZipFile.FromOriginal(KenAll_Local_Path);
+
+            DataCreator.CreateDataCsvFiles(originalData);
         }
     }
 }
