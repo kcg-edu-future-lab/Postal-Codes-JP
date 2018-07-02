c3384933e280c78d699f6922c740dca0c6f42b71
Keiho Sakapon <git@saka-pon.net>
2018-07-02 17:42:55 +0900

    Create project

---
+++ PostalCodes/PostalCodesDataConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PostalCodesDataConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
