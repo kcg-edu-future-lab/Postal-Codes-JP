769e26032f413ebd9dcb9c71b9fe891d17c670fe
Keiho Sakapon <git@saka-pon.net>
2018-07-04 12:32:44 +0900

    Change signatures

--- PostalCodes/PostalCodesWebApi/Controllers/PostalCodesController.cs
+++ PostalCodes/PostalCodesWebApi/Controllers/PostalCodesController.cs
@@ -63,31 +63,5 @@ namespace PostalCodesWebApi.Controllers
             return PostalCodesData.Towns.Where(x => x.PostalCode.StartsWith(postalCode))
                 .OrderBy(x => x.PostalCode);
         }
-
-        /// <summary>
-        /// 町域の名前を指定して、郵便番号と町域のリストを取得します。部分一致検索です。
-        /// </summary>
-        /// <param name="name">町域の名前</param>
-        /// <returns>郵便番号と町域のリスト</returns>
-        [HttpGet("ByName/{name}")]
-        public IEnumerable<Town> GetByName(string name)
-        {
-            if (string.IsNullOrWhiteSpace(name)) return Enumerable.Empty<Town>();
-
-            return PostalCodesData.Towns.Where(x => x.Name.Contains(name));
-        }
-
-        /// <summary>
-        /// 町域のかなを指定して、郵便番号と町域のリストを取得します。部分一致検索です。
-        /// </summary>
-        /// <param name="kana">町域のかな</param>
-        /// <returns>郵便番号と町域のリスト</returns>
-        [HttpGet("ByKana/{kana}")]
-        public IEnumerable<Town> GetByKana(string kana)
-        {
-            if (string.IsNullOrWhiteSpace(kana)) return Enumerable.Empty<Town>();
-
-            return PostalCodesData.Towns.Where(x => x.Kana.Contains(kana));
-        }
     }
 }
---
+++ PostalCodes/PostalCodesWebApi/Controllers/TownsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PostalCodesWebApi.Models;
+
+namespace PostalCodesWebApi.Controllers
+{
+    /// <summary>
+    /// 郵便番号と町域のデータを取得します。
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TownsController : Controller
+    {
+        /// <summary>
+        /// 町域の名前、かなを指定して、郵便番号と町域のリストを取得します。部分一致検索です。
+        /// </summary>
+        /// <param name="name">町域の名前</param>
+        /// <param name="kana">町域のかな</param>
+        /// <returns>郵便番号と町域のリスト</returns>
+        [HttpGet]
+        public IEnumerable<Town> Get(string name, string kana)
+        {
+            IEnumerable<Town> query = PostalCodesData.Towns;
+
+            if (!string.IsNullOrWhiteSpace(name))
+                query = query.Where(x => x.Name.Contains(name));
+            if (!string.IsNullOrWhiteSpace(kana))
+                query = query.Where(x => x.Kana.Contains(kana));
+
+            return query;
+        }
+    }
+}
