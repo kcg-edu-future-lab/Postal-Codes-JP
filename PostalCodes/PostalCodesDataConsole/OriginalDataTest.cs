d8acaac8166e067aca9a1f48967e1eb394327c7c
Keiho Sakapon <git@saka-pon.net>
2018-07-08 12:38:09 +0900

    Add test

---
+++ PostalCodes/PostalCodesDataConsole/OriginalDataTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Bellona.IO;
+
+namespace PostalCodesDataConsole
+{
+    public static class OriginalDataTest
+    {
+        static string[][] OriginalData;
+
+        public static void ExecuteTest()
+        {
+            OriginalData = DataZipFile.FromOriginal(Program.KenAll_Local_Path);
+            Directory.CreateDirectory(nameof(OriginalDataTest));
+
+            SaveResult(nameof(MultiPostalCodes), MultiPostalCodes());
+            SaveResult(nameof(MultiTowns), MultiTowns());
+        }
+
+        static void SaveResult(string fileName, IEnumerable<string[]> result) =>
+            CsvFile.WriteRecordsByArray(Path.Combine(nameof(OriginalDataTest), $"{fileName}.csv"), result, Encoding.UTF8);
+
+        static IEnumerable<string[]> MultiPostalCodes() => OriginalData
+            .Where(l => l[9] != "0");
+
+        static IEnumerable<string[]> MultiTowns() => OriginalData
+            .Where(l => l[12] != "0");
+    }
+}
--- PostalCodes/PostalCodesDataConsole/Program.cs
+++ PostalCodes/PostalCodesDataConsole/Program.cs
@@ -4,11 +4,11 @@ namespace PostalCodesDataConsole
 {
     class Program
     {
-        const string KenAll_Local_Path = @"..\..\..\..\..\Data\Original\201805\ken_all.zip";
-        const string Kyoto_Local_Path = @"..\..\..\..\..\Data\Original\201805\26kyouto.zip";
-        const string KenAll_JP_Uri = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip";
+        public const string KenAll_Local_Path = @"..\..\..\..\..\Data\Original\201805\ken_all.zip";
+        public const string Kyoto_Local_Path = @"..\..\..\..\..\Data\Original\201805\26kyouto.zip";
+        public const string KenAll_JP_Uri = "http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip";
 
-        const string PostalCodesData_Path = @"..\..\..\..\..\Data\Remodeled\201805\PostalCodesData.zip";
+        public const string PostalCodesData_Path = @"..\..\..\..\..\Data\Remodeled\201805\PostalCodesData.zip";
 
         static void Main(string[] args)
         {
