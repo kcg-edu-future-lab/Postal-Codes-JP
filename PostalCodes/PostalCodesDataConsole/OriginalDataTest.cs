bb6cd02b1dbfa213e6598e482a565581ece11c8e
Keiho Sakapon <git@saka-pon.net>
2018-07-09 22:38:01 +0900

    Add test

--- PostalCodes/PostalCodesDataConsole/OriginalDataTest.cs
+++ PostalCodes/PostalCodesDataConsole/OriginalDataTest.cs
@@ -18,6 +18,9 @@ namespace PostalCodesDataConsole
 
             SaveResult(nameof(MultiPostalCodes), MultiPostalCodes());
             SaveResult(nameof(MultiTowns), MultiTowns());
+            SaveResult(nameof(ConsecutiveData), ConsecutiveData());
+            SaveResult(nameof(ConsecutiveData_Reason), ConsecutiveData_Reason());
+            SaveResult(nameof(ConsecutiveData_Kana), ConsecutiveData_Kana());
         }
 
         static void SaveResult(string fileName, IEnumerable<string[]> result) =>
@@ -28,5 +31,29 @@ namespace PostalCodesDataConsole
 
         static IEnumerable<string[]> MultiTowns() => OriginalData
             .Where(l => l[12] != "0");
+
+        // 郵便番号が連続しているデータのうち、その理由が文字数超過かつ複数町域のものを取得します。
+        static IEnumerable<string[]> ConsecutiveData() => OriginalData
+            .GroupBySequentially(l => l[2])
+            .Where(g => g.Count() > 1)
+            .Where(g => g.First()[8].Contains("（") && !g.First()[8].Contains("）"))
+            .Where(g => g.First()[12] != "0")
+            .SelectMany(g => g);
+
+        // 郵便番号が連続している理由 (「複数町域」か、「単町域かつ文字数超過」か) が混在して連続しないことを保証します。
+        static IEnumerable<string[]> ConsecutiveData_Reason() => OriginalData
+            .GroupBySequentially(l => l[2])
+            .Where(g => g.Count() > 1)
+            .Where(g => g.Select(l => l[12]).Distinct().Count() > 1)
+            .SelectMany(g => g);
+
+        // 文字数超過により郵便番号が連続している場合のかなを検証します。
+        // 各行で名前とかなは対応しているようです。
+        static IEnumerable<string[]> ConsecutiveData_Kana() => OriginalData
+            .GroupBySequentially(l => l[2])
+            .Where(g => g.Count() > 1)
+            .Where(g => g.First()[8].Contains("（") && !g.First()[8].Contains("）"))
+            .Where(g => g.Select(l => l[5]).Distinct().Count() > 1)
+            .SelectMany(g => g);
     }
 }
