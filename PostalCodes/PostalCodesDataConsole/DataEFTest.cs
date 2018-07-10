bbc21e33a1138328da98d824b41c174882a9aa5c
Keiho Sakapon <git@saka-pon.net>
2018-07-10 13:10:58 +0900

    Add test

--- PostalCodes/PostalCodesDataConsole/DataEFTest.cs
+++ PostalCodes/PostalCodesDataConsole/DataEFTest.cs
@@ -15,6 +15,7 @@ namespace PostalCodesDataConsole
         {
             Directory.CreateDirectory(OutputDirPath);
 
+            SaveResult(nameof(TownNames_Empty), TownNames_Empty());
             SaveResult(nameof(MultiTowns_Max), MultiTowns_Max());
             SaveResult(nameof(SingleChars), SingleChars());
         }
@@ -25,6 +26,22 @@ namespace PostalCodesDataConsole
         static string Hyphenate(this string postalCode) =>
             postalCode.Insert(3, "-");
 
+        static IEnumerable<string> TownNames_Empty()
+        {
+            using (var db = new PostalCodesDb())
+            {
+                var towns = db.Towns.Include("City.Pref")
+                    .Where(x => x.Name == "")
+                    .ToArray();
+
+                return towns
+                    .GroupBy(x => x.PostalCode)
+                    .Where(g => g.Count() > 1 || g.First().Remarks != "お探しの町域が見つからない場合")
+                    .SelectMany(g => g)
+                    .Select(x => $"{x.PostalCode.Hyphenate()} {x.Index:D2}: {x.City.Pref.Name} {x.City.Name} {x.Remarks}");
+            }
+        }
+
         static IEnumerable<string> MultiTowns_Max()
         {
             using (var db = new PostalCodesDb())
@@ -34,7 +51,7 @@ namespace PostalCodesDataConsole
 
                 return maxIndexTowns
                     .SelectMany(mt => db.Towns.Include("City.Pref").Where(x => x.PostalCode == mt.PostalCode))
-                    .Select(x => $"{x.PostalCode.Hyphenate()} {x.Index.ToString("D2")}: {x.City.Pref.Name} {x.City.Name} {x.Name}")
+                    .Select(x => $"{x.PostalCode.Hyphenate()} {x.Index:D2}: {x.City.Pref.Name} {x.City.Name} {x.Name}")
                     .ToArray();
             }
         }
@@ -54,7 +71,7 @@ namespace PostalCodesDataConsole
                 var chars = texts
                     .SelectMany(_ => _.name.Select(c => new { c, _.text }))
                     .GroupBy(_ => _.c)
-                    .Where(g => g.Count() == 1)
+                    .Where(g => g.Count() <= 1)
                     .SelectMany(g => g)
                     .ToArray();
 
